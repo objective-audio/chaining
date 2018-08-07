eb25b5b884e92eae917e205ee492f8ca9bb21cd0
Yuki Yasoshima <yukiyasos@gmail.com>
2018-08-07 11:45:31 +0900

    add test project

---
+++ chaining_test/chaining_test_mac/chaining_test_mac.mm
@@ -0,0 +1,21 @@
+//
+//  chaining_test_mac.mm
+//
+
+#import <XCTest/XCTest.h>
+
+@interface chaining_test_mac : XCTestCase
+
+@end
+
+@implementation chaining_test_mac
+
+- (void)setUp {
+    [super setUp];
+}
+
+- (void)tearDown {
+    [super tearDown];
+}
+
+@end
