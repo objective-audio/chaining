7588d69a8cc3c97402c2aed9bbf0d2affa92470c
Yuki Yasoshima <yukiyasos@gmail.com>
2018-08-13 10:34:04 +0900

    separate receiver_chainable_private

--- chaining/yas_chaining_chain_private.h
+++ chaining/yas_chaining_chain_private.h
@@ -7,29 +7,10 @@
 #include <mutex>
 #include <unordered_map>
 #include <vector>
+#include "yas_chaining_receiver_chainable_private.h"
 #include "yas_fast_each.h"
 
 namespace yas::chaining {
-
-#pragma mark - chaining::receiver_chainable
-
-template <typename T>
-receiver_chainable<T>::receiver_chainable(std::shared_ptr<impl> impl) : protocol(std::move(impl)) {
-}
-
-template <typename T>
-receiver_chainable<T>::receiver_chainable(std::nullptr_t) : protocol(nullptr) {
-}
-
-template <typename T>
-void receiver_chainable<T>::receive_value(T const &value) {
-    impl_ptr<impl>()->receive_value(value);
-}
-
-template <typename T>
-output<T> receiver_chainable<T>::make_output() {
-    return impl_ptr<impl>()->make_output();
-}
 
 #pragma mark - sender_chainable
 
---
+++ chaining/yas_chaining_receiver_chainable_private.h
@@ -0,0 +1,25 @@
+//
+//  yas_chaining_receiver_chainable_private.h
+//
+
+#pragma once
+
+namespace yas::chaining {
+template <typename T>
+receiver_chainable<T>::receiver_chainable(std::shared_ptr<impl> impl) : protocol(std::move(impl)) {
+}
+
+template <typename T>
+receiver_chainable<T>::receiver_chainable(std::nullptr_t) : protocol(nullptr) {
+}
+
+template <typename T>
+void receiver_chainable<T>::receive_value(T const &value) {
+    impl_ptr<impl>()->receive_value(value);
+}
+
+template <typename T>
+output<T> receiver_chainable<T>::make_output() {
+    return impl_ptr<impl>()->make_output();
+}
+}  // namespace yas::chaining
