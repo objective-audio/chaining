08a70c457c273f551fb5f7aecfa7d23456c59995
Yuki Yasoshima <yukiyasos@gmail.com>
2018-08-13 12:36:24 +0900

    remove output_private

--- chaining/yas_chaining_protocol.h
+++ chaining/yas_chaining_protocol.h
@@ -7,8 +7,3 @@
 #include "yas_base.h"
 #include "yas_chaining_receiver_chainable.h"
 #include "yas_chaining_sender_chainable.h"
-
-namespace yas::chaining {
-}  // namespace yas::chaining
-
-#include "yas_chaining_output_private.h"
--- chaining/yas_chaining_receiver_chainable_private.h
+++ chaining/yas_chaining_receiver_chainable_private.h
@@ -6,6 +6,33 @@
 
 namespace yas::chaining {
 template <typename T>
+struct output<T>::impl : base::impl {
+    weak<receiver<T>> _weak_receiver;
+
+    impl(weak<receiver<T>> &&weak_receiver) : _weak_receiver(std::move(weak_receiver)) {
+    }
+
+    void output_value(T const &value) {
+        if (auto receiver = this->_weak_receiver.lock()) {
+            receiver.chainable().receive_value(value);
+        }
+    }
+};
+
+template <typename T>
+output<T>::output(weak<receiver<T>> weak_receiver) : base(std::make_shared<impl>(std::move(weak_receiver))) {
+}
+
+template <typename T>
+output<T>::output(std::nullptr_t) : base(nullptr) {
+}
+
+template <typename T>
+void output<T>::output_value(T const &value) {
+    impl_ptr<impl>()->output_value(value);
+}
+
+template <typename T>
 receiver_chainable<T>::receiver_chainable(std::shared_ptr<impl> impl) : protocol(std::move(impl)) {
 }
 
