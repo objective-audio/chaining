a67b1d332b3fd528b70175297b944470ebbde1b7
Yuki Yasoshima <yukiyasos@gmail.com>
2018-08-14 23:09:58 +0900

    tests

---
+++ chaining_test/chaining_test_mac/yas_sync_tests.mm
@@ -0,0 +1,105 @@
+//
+//  yas_sync_tests.mm
+//
+
+#import <XCTest/XCTest.h>
+#import "yas_chaining.h"
+
+using namespace yas;
+
+@interface yas_sync_tests : XCTestCase
+
+@end
+
+@implementation yas_sync_tests
+
+- (void)setUp {
+    [super setUp];
+}
+
+- (void)tearDown {
+    [super tearDown];
+}
+
+- (void)test_sync {
+    chaining::fetcher<int> fetcher{[] { return 100; }};
+
+    int received = -1;
+
+    auto chain = fetcher.chain().perform([&received](int const &value) { received = value; }).end();
+    chain.sync();
+
+    XCTAssertEqual(received, 100);
+}
+
+- (void)test_sync_by_observer {
+    chaining::fetcher<int> fetcher{[] { return 100; }};
+
+    int received = -1;
+
+    chaining::observer chain = fetcher.chain().perform([&received](int const &value) { received = value; }).end();
+    chain.sync();
+
+    XCTAssertEqual(received, 100);
+}
+
+- (void)test_sync_many_chain {
+    chaining::fetcher<int> fetcher{[] { return 100; }};
+
+    int received1 = -1;
+    int received2 = -1;
+
+    auto chain1 = fetcher.chain().perform([&received1](int const &value) { received1 = value; }).end();
+    auto chain2 = fetcher.chain().perform([&received2](int const &value) { received2 = value; }).end();
+
+    chain1.sync();
+
+    XCTAssertEqual(received1, 100);
+    XCTAssertEqual(received2, -1);
+}
+
+- (void)test_sync_end {
+    chaining::fetcher<int> fetcher{[] { return 100; }};
+
+    int received = -1;
+
+    auto chain = fetcher.chain().perform([&received](int const &value) { received = value; }).sync();
+
+    XCTAssertEqual(received, 100);
+}
+
+- (void)test_sync_with_combined_sub_sender {
+    chaining::fetcher<int> fetcher{[]() { return 123; }};
+    chaining::fetcher<int> sub_fetcher{[]() { return 456; }};
+
+    std::vector<std::pair<int, int>> received;
+
+    auto chain = fetcher.chain()
+                     .combine(sub_fetcher.chain())
+                     .perform([&received](auto const &pair) { received.emplace_back(pair); })
+                     .sync();
+
+    // 2つのsenderから来た値が両方揃ってから受け取った
+    XCTAssertEqual(received.size(), 1);
+    XCTAssertEqual(received.at(0).first, 123);
+    XCTAssertEqual(received.at(0).second, 456);
+}
+
+- (void)test_sync_with_merged_sub_sender {
+    chaining::fetcher<int> fetcher{[]() { return 78; }};
+    chaining::fetcher<int> sub_fetcher{[]() { return 90; }};
+
+    std::vector<int> received;
+
+    auto chain = fetcher.chain()
+                     .merge(sub_fetcher.chain())
+                     .perform([&received](int const &value) { received.emplace_back(value); })
+                     .sync();
+
+    // main -> sub の順番で実行される
+    XCTAssertEqual(received.size(), 2);
+    XCTAssertEqual(received.at(0), 78);
+    XCTAssertEqual(received.at(1), 90);
+}
+
+@end
---
+++ chaining_test/chaining_test_mac/yas_to_tests.mm
@@ -0,0 +1,130 @@
+//
+//  yas_to_tests.mm
+//
+
+#import <XCTest/XCTest.h>
+#import <string>
+#import "yas_chaining.h"
+
+using namespace yas;
+
+@interface yas_to_tests : XCTestCase
+
+@end
+
+@implementation yas_to_tests
+
+- (void)setUp {
+    [super setUp];
+}
+
+- (void)tearDown {
+    [super tearDown];
+}
+
+- (void)test_to {
+    chaining::notifier<int> notifier;
+
+    int received = -1;
+
+    auto chain = notifier.chain()
+                     .to([](int const &value) { return value + 1; })
+                     .perform([&received](int const &value) { received = value; })
+                     .end();
+
+    notifier.notify(10);
+
+    XCTAssertEqual(received, 11);
+}
+
+- (void)test_to_type {
+    chaining::notifier<int> notifier;
+
+    std::string received = "";
+
+    auto chain = notifier.chain()
+                     .guard([](int const &) { return true; })
+                     .to([](int const &value) { return value > 0; })
+                     .guard([](bool const &) { return true; })
+                     .to([](bool const &value) { return value ? "true" : "false"; })
+                     .guard([](std::string const &) { return true; })
+                     .perform([&received](std::string const &value) { received = value; })
+                     .end();
+
+    notifier.notify(0);
+
+    XCTAssertEqual(received, "false");
+
+    notifier.notify(1);
+
+    XCTAssertEqual(received, "true");
+}
+
+- (void)test_to_value {
+    chaining::notifier<int> notifier;
+
+    std::string received = "";
+
+    auto chain = notifier.chain()
+                     .to_value(std::string("test_value"))
+                     .perform([&received](std::string const &value) { received = value; })
+                     .end();
+
+    notifier.notify(0);
+
+    XCTAssertEqual(received, "test_value");
+}
+
+- (void)test_to_null {
+    chaining::notifier<int> notifier;
+
+    bool called = false;
+
+    auto chain = notifier.chain().to_null().perform([&called](std::nullptr_t const &) { called = true; }).end();
+
+    notifier.notify(1);
+
+    XCTAssertTrue(called);
+}
+
+- (void)test_to_tuple {
+    chaining::notifier<int> notifier;
+
+    opt_t<std::tuple<int>> called;
+
+    auto chain = notifier.chain().to_tuple().perform([&called](std::tuple<int> const &value) { called = value; }).end();
+
+    notifier.notify(1);
+
+    XCTAssertTrue(called);
+    XCTAssertEqual(std::get<0>(*called), 1);
+}
+
+- (void)test_to_tuple_from_tuple {
+    chaining::notifier<std::tuple<int>> notifier;
+
+    opt_t<std::tuple<int>> called;
+
+    auto chain = notifier.chain().to_tuple().perform([&called](auto const &value) { called = value; }).end();
+
+    notifier.notify(std::make_tuple(int(1)));
+
+    XCTAssertTrue(called);
+    XCTAssertEqual(std::get<0>(*called), 1);
+}
+
+- (void)test_to_tuple_from_pair {
+    chaining::notifier<std::pair<int, std::string>> notifier;
+
+    opt_t<std::tuple<int, std::string>> called;
+
+    auto chain = notifier.chain().to_tuple().perform([&called](auto const &value) { called = value; }).end();
+
+    notifier.notify(std::make_pair(int(1), std::string("2")));
+
+    XCTAssertTrue(called);
+    XCTAssertEqual(std::get<0>(*called), 1);
+    XCTAssertEqual(std::get<1>(*called), "2");
+}
+
+@end
