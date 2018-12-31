773e50056e6bae6621ff212549794b9063d65bdb
Yuki Yasoshima <yukiyasos@gmail.com>
2018-12-31 11:51:33 +0900

    add chaining_types

--- chaining/yas_chaining_chain.h
+++ chaining/yas_chaining_chain.h
@@ -61,19 +61,6 @@ struct[[nodiscard]] chain : base {
     observer<Begin> end();
     observer<Begin> sync();
 };
-
-template <typename T>
-using chain_sync_t = chain<T, T, T, true>;
-template <typename T, typename U>
-using chain_relayed_sync_t = chain<T, U, U, true>;
-template <typename T, typename U>
-using chain_normalized_sync_t = chain<T, T, U, true>;
-template <typename T>
-using chain_unsync_t = chain<T, T, T, false>;
-template <typename T, typename U>
-using chain_relayed_unsync_t = chain<T, U, U, false>;
-template <typename T, typename U>
-using chain_normalized_unsync_t = chain<T, T, U, false>;
 }  // namespace yas::chaining
 
 #include "yas_chaining_chain_private.h"
