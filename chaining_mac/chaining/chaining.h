5275100f478a5a6465d0af648061c8df3df13dbc
Yuki Yasoshima <yukiyasos@gmail.com>
2019-01-02 10:41:45 +0900

    update framework

---
+++ chaining_mac/chaining/chaining.h
@@ -0,0 +1,11 @@
+//
+//  chaining.h
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for chaining.
+FOUNDATION_EXPORT double chainingVersionNumber;
+
+//! Project version string for chaining.
+FOUNDATION_EXPORT const unsigned char chainingVersionString[];
