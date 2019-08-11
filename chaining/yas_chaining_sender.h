374e4c26bf3dc1ffbf0550ad50f0f2a3bb211aec
Yuki Yasoshima <yukiyasos@gmail.com>
2019-08-11 18:11:43 +0900

    sender implementation

--- chaining/yas_chaining_sender.h
+++ chaining/yas_chaining_sender.h
@@ -20,50 +20,20 @@ struct sender : any_sender, std::enable_shared_from_this<sender<T>> {
 
     virtual void fetch_for(any_joint const &joint) = 0;
 
-    void broadcast(T const &value) {
-        for (std::weak_ptr<joint<T>> const &weak_joint : this->_joints) {
-            if (joint_ptr<T> joint = weak_joint.lock()) {
-                joint->call_first(value);
-            }
-        }
-    }
+    void broadcast(T const &value);
+    void erase_joint(std::uintptr_t const key);
 
-    void erase_joint(std::uintptr_t const key) {
-        erase_if(this->_joints, [key](auto const &weak_joint) {
-            if (auto joint = weak_joint.lock()) {
-                return joint->identifier() == key;
-            } else {
-                return false;
-            }
-        });
-    }
-
-    chain_unsync_t<T> chain_unsync() {
-        return this->_chain<false>();
-    }
-
-    chain_sync_t<T> chain_sync() {
-        return this->_chain<true>();
-    }
+    chain_unsync_t<T> chain_unsync();
+    chain_sync_t<T> chain_sync();
 
    protected:
-    void send_value_to_target(T const &value, std::uintptr_t const key) {
-        for (std::weak_ptr<joint<T>> const &weak_joint : this->_joints) {
-            if (joint_ptr<T> joint = weak_joint.lock(); joint && joint->identifier() == key) {
-                joint->call_first(value);
-            }
-        }
-    }
+    void send_value_to_target(T const &value, std::uintptr_t const key);
 
    private:
     std::vector<std::weak_ptr<joint<T>>> _joints;
 
     template <bool Syncable>
-    chain<T, T, Syncable> _chain() {
-        auto joint = make_joint(to_weak(this->shared_from_this()));
-        this->_joints.emplace_back(to_weak(joint));
-        return chaining::chain<T, T, Syncable>{std::move(joint)};
-    }
+    chain<T, T, Syncable> _chain();
 };
 
 template <typename T>
