1d443bc4db8186ff08f1d902c98f68bf12520540
Yuki Yasoshima <yukiyasos@gmail.com>
2019-12-21 14:29:04 +0900

    move invalidate tests

--- chaining_test/yas_observer_tests.mm
+++ chaining_test/yas_observer_tests.mm
@@ -3,6 +3,7 @@
 //
 
 #import <XCTest/XCTest.h>
+#import <chaining/yas_chaining_notifier.h>
 #import <chaining/yas_chaining_observer.h>
 #import <chaining/yas_chaining_value_holder.h>
 
@@ -61,6 +62,51 @@ using namespace yas::chaining;
     sub_holder->set_value(4);
 
     XCTAssertEqual(received, 2);
+}
+
+- (void)test_invalidate_on_perform {
+    struct observer_holder {
+        std::optional<chaining::any_observer_ptr> observer = std::nullopt;
+    };
+
+    auto holder = std::make_shared<observer_holder>();
+    auto notifier = chaining::notifier<int>::make_shared();
+
+    auto expectation = [self expectationWithDescription:@"not called"];
+    expectation.inverted = YES;
+
+    holder->observer = notifier->chain()
+                           .perform([&holder](int const &) { holder->observer.value()->invalidate(); })
+                           .perform([expectation](int const &) { [expectation fulfill]; })
+                           .end();
+
+    notifier->notify(0);
+
+    [self waitForExpectations:@[expectation] timeout:0.0];
+}
+
+- (void)test_invalidate_on_to {
+    struct observer_holder {
+        std::optional<chaining::any_observer_ptr> observer = std::nullopt;
+    };
+
+    auto holder = std::make_shared<observer_holder>();
+    auto notifier = chaining::notifier<int>::make_shared();
+
+    auto expectation = [self expectationWithDescription:@"not called"];
+    expectation.inverted = YES;
+
+    holder->observer = notifier->chain()
+                           .to([&holder](int const &value) {
+                               holder->observer.value()->invalidate();
+                               return std::to_string(value);
+                           })
+                           .perform([expectation](std::string const &) { [expectation fulfill]; })
+                           .end();
+
+    notifier->notify(0);
+
+    [self waitForExpectations:@[expectation] timeout:0.0];
 }
 
 @end
