2c2e630036e8142cca94d47e03df8501e19f4c8a
Yuki Yasoshima <yukiyasos@gmail.com>
2021-01-27 18:46:43 +0900

    test
    test

--- chaining_test/yas_observing_invalidator_pool_tests.mm
+++ chaining_test/yas_observing_invalidator_pool_tests.mm
@@ -87,4 +87,56 @@ using namespace yas::observing;
     XCTAssertEqual(called.size(), 1);
 }
 
+- (void)test_add_to {
+    std::vector<int> called;
+
+    auto const notifier = observing::notifier<int>::make_shared();
+
+    auto pool1 = invalidator_pool::make_shared();
+    invalidator_pool pool2;
+
+    notifier->observe([&called](int const &value) { called.emplace_back(value); })->add_to(*pool1);
+
+    pool1->add_to(pool2);
+
+    XCTAssertEqual(called.size(), 0);
+
+    notifier->notify(3);
+
+    XCTAssertEqual(called.size(), 1);
+    XCTAssertEqual(called.at(0), 3);
+
+    pool2.invalidate();
+
+    notifier->notify(4);
+
+    XCTAssertEqual(called.size(), 1);
+}
+
+- (void)test_set_to {
+    std::vector<int> called;
+
+    auto const notifier = observing::notifier<int>::make_shared();
+
+    auto pool = invalidator_pool::make_shared();
+    invalidatable_ptr invalidator = nullptr;
+
+    notifier->observe([&called](int const &value) { called.emplace_back(value); })->add_to(*pool);
+
+    pool->set_to(invalidator);
+
+    XCTAssertEqual(called.size(), 0);
+
+    notifier->notify(3);
+
+    XCTAssertEqual(called.size(), 1);
+    XCTAssertEqual(called.at(0), 3);
+
+    invalidator->invalidate();
+
+    notifier->notify(4);
+
+    XCTAssertEqual(called.size(), 1);
+}
+
 @end
