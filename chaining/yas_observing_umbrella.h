a216429ad9c76215d250602d9cc20bd216756365
Yuki Yasoshima <yukiyasos@gmail.com>
2021-02-27 12:43:55 +0900

    map_holder

--- chaining/yas_observing_umbrella.h
+++ chaining/yas_observing_umbrella.h
@@ -8,6 +8,7 @@
 #include <chaining/yas_observing_canceller.h>
 #include <chaining/yas_observing_canceller_pool.h>
 #include <chaining/yas_observing_fetcher.h>
+#include <chaining/yas_observing_map_holder.h>
 #include <chaining/yas_observing_notifier.h>
 #include <chaining/yas_observing_value_holder.h>
 #include <chaining/yas_observing_vector_holder.h>
